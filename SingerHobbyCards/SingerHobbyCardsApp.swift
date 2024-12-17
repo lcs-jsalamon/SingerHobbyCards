86ea69d28209babe7a0a12cd49f57df1cc09e926
Josephine Salamon <jsalamon@lcs.on.ca>
2024-12-17 08:40:56 -0500

    Initial Commit

---
+++ SingerHobbyCards/SingerHobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingerHobbyCardsApp.swift
+//  SingerHobbyCards
+//
+//  Created by Josephine Salamon on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct SingerHobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SingerHobbyCardsTests/SingerHobbyCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SingerHobbyCardsTests.swift
+//  SingerHobbyCardsTests
+//
+//  Created by Josephine Salamon on 2024-12-17.
+//
+
+import Testing
+@testable import SingerHobbyCards
+
+struct SingerHobbyCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
