533157593d10d8ad6cd2aeacbd9e73f1a82cc46e
Josephine Salamon <jsalamon@lcs.on.ca>
2024-12-18 12:23:29 -0500

    made all the colours for each singer

--- SingerHobbyCards/Model/Singer.swift
+++ SingerHobbyCards/Model/Singer.swift
@@ -22,9 +22,9 @@ struct Singer: Identifiable {
 
 
 let sza = Singer(singerName: "SZA", monthlyListeners: "79,529,710", topSong: "1. luther (with sza)", secondSong: "2. Kill Bill", thirdSong: "3. Snooze", imageName: "szaPfp", primaryColor: .szaMidBlue)
-let pinkPantheress = Singer(singerName: "Pink Pantheress", monthlyListeners: "18,626,440", topSong: "1. Pain", secondSong:"2. Boy's a liar Pt.2", thirdSong: "3. I Must Apologise", imageName: "pinkPfp", primaryColor: .blue)
-let gracieAbrams = Singer(singerName: "Gracie Abrams", monthlyListeners: "43,443,777", topSong: "1. That's So True", secondSong: "2. I Love You, I'm Sorry", thirdSong: "3. Close To You", imageName: "graciePfp", primaryColor: .pink)
-let meganMoroney = Singer(singerName: "Megan Moroney", monthlyListeners: "8,635,112", topSong: "1. Tennesse Orange", secondSong:"2. Am I Okay?", thirdSong: "3. I'm Not Pretty", imageName: "meganPfp", primaryColor: .green)
-let phoebeBridgers = Singer(singerName: "Phoebe Bridgers", monthlyListeners: "12,938,861", topSong: "1. Scott Street", secondSong: "2. Motion Sickness", thirdSong: "3. Waiting Room", imageName: "phoebePfp", primaryColor: .brown)
+let pinkPantheress = Singer(singerName: "Pink Pantheress", monthlyListeners: "18,626,440", topSong: "1. Pain", secondSong:"2. Boy's a liar Pt.2", thirdSong: "3. I Must Apologise", imageName: "pinkPfp", primaryColor: .pinkMidPink)
+let gracieAbrams = Singer(singerName: "Gracie Abrams", monthlyListeners: "43,443,777", topSong: "1. That's So True", secondSong: "2. I Love You, I'm Sorry", thirdSong: "3. Close To You", imageName: "graciePfp", primaryColor: .gracieMidGreen)
+let meganMoroney = Singer(singerName: "Megan Moroney", monthlyListeners: "8,635,112", topSong: "1. Tennesse Orange", secondSong:"2. Am I Okay?", thirdSong: "3. I'm Not Pretty", imageName: "meganPfp", primaryColor: .meganMidBlue)
+let phoebeBridgers = Singer(singerName: "Phoebe Bridgers", monthlyListeners: "12,938,861", topSong: "1. Scott Street", secondSong: "2. Motion Sickness", thirdSong: "3. Waiting Room", imageName: "phoebePfp", primaryColor: .phoebeMidBeige)
 
 let singerList = [sza,pinkPantheress,gracieAbrams,meganMoroney,phoebeBridgers]
